6e5f8d55f1df1fab0a30b2006d00dfc9f8c90545
klopex <klopex@klopex.com>
2018-04-01 23:15:52 -0400

    I fixed Linux keyboard input.  I had to add a second thread to consume the keyboard input.  I took out some clever traits of the keyboard input code to make it more straight code.  Now it is not using an exception as an if statement.  It is not using the impl of a parent class and the __call__ function to hide what is going on.  It is more verbose, but I can have multiple functions so I can stop the threads.

--- Downhill.py
+++ Downhill.py
@@ -1,5 +1,6 @@
 import time
 import random
+from sys import platform
 
 ROW_WIDTH = 75
 ski_appearance = ['//', '||', '\\\\', 'XX']
@@ -7,45 +8,80 @@ LEFT = 0
 STRAIGHT = 1
 RIGHT = 2
 CRASH = 3
+QUIT = 4
 
 # The following Getch stuff is based on code I copied from StackOverflow
 
-class _Getch:
+class Getch:
     """Gets a single character from standard input.  Does not echo to the
 screen."""
     def __init__(self):
-        try:
+        if platform == 'win32':
             self.impl = _GetchWindows()
-        except ImportError:
+        elif platform == 'linux' or platform == 'linux2':
             self.impl = _GetchUnix()
+        else:
+            raise Exception('ERROR: This only works on Linux or Windows!')
 
-    def __call__(self):
-        return self.impl()
+    def getch(self):
+        return self.impl.myGetch()
 
-#TODO Test _GetchUnix on Linux.  I do not know what it will do, but
-#     I fear that it will wait on user input.
+    def cleanup(self):
+        self.impl.stopThread()
+
 
 class _GetchUnix:
     def __init__(self):
-        import tty, sys
+        import tty
+        import sys
+        import threading
+        self.keepRunning = True
+        self.char = ''
+        def getCharThread(arg):
+            import sys, tty, termios
+            fd = sys.stdin.fileno()
+            while(self.keepRunning):
+                old_settings = termios.tcgetattr(fd)
+                try:
+                    tty.setraw(sys.stdin.fileno())
+                    ch = sys.stdin.read(1)
+                    # Arrow keys on Linux come in as three separate bytes.  chr(27), '['==chr(91) and either
+                    # 'C'==chr(67) for Right arrow, or
+                    # 'D'==chr(68) for Left arrow.
+                    # The way sys.stdin.read(1) works, one key press returns a one-byte strings in three loops.
+                    if (ord(ch) == 27):
+                        ch = sys.stdin.read(1)
+                        if (ord(ch) == 91):
+                            ch = sys.stdin.read(1)
+                finally:
+                    termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+                self.char = ch
+        # I had to make this multithreaded so that the main thread would not
+        # block while waiting on user input.
+        self.myThread = threading.Thread(target=getCharThread, args=('',))
+        self.myThread.start()
 
-    def __call__(self):
-        import sys, tty, termios
-        fd = sys.stdin.fileno()
-        old_settings = termios.tcgetattr(fd)
-        try:
-            tty.setraw(sys.stdin.fileno())
-            ch = sys.stdin.read(1)
-        finally:
-            termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
-        return ch
+
+    def stopThread(self):
+        #print('stopThread called')
+        self.keepRunning = False
+        self.myThread.join()
+
+    def myGetch(self):
+        x = self.char
+        self.char = '_'
+        return x
 
 
 class _GetchWindows:
     def __init__(self):
         import msvcrt
 
-    def __call__(self):
+    def stopThread(self):
+        #print('stopThread called')
+        pass
+
+    def myGetch(self):
         import msvcrt
         kbhit = msvcrt.kbhit()
         if kbhit:
@@ -62,11 +98,23 @@ class _GetchWindows:
 
 # The following is all my code.
 
+def myPrint(theString):
+    '''The Linux input consumption somehow removes the carriage return trait from my
+       prints.  It linefeeds, but it does not return to column zero.
+       This function compensates for that.  I have to use it after I start
+       grabbing user input.
+       '''
+    if platform=='linux' or platform == 'linux2':
+        # For some reason, the input logic takes away the carriage return
+        # so I have to add one back in.  Not a linefeed, but a carrage return...
+        theString = '\r' + theString
+    print(theString)
+
 def clear_screen():
     ''' Clears the screen by printing blank lines
         '''
     for i in range(0,30):
-        print('')
+        myPrint('')
 
 
 def build_row(center, lane_width):
@@ -99,6 +147,8 @@ def handle_movement(ski_position, ski_direction):
         ski_position = ski_position - 1
     elif ski_direction == RIGHT:
         ski_position = ski_position + 1
+    elif ski_direction == QUIT:
+        ski_position = 0
     return ski_position
 
 def get_hill_info():
@@ -116,12 +166,23 @@ def get_movement():
         Return either LEFT, STRAIGHT (down), or RIGHT.
         '''
     retval = STRAIGHT
-    x = getch()
+    x = inText.getch()
     if len(x) > 0:
-        if (x[0] == 224) and (x[1] == 75):
-            retval = LEFT
-        elif (x[0] == 224) and (x[1] == 77):
-            retval = RIGHT
+        #myPrint(ord(x))
+        if len(x) > 1:
+            # Arrow keys are two characters long on Windows
+            if (x[0] == 224) and (x[1] == 75):
+                retval = LEFT
+            elif (x[0] == 224) and (x[1] == 77):
+                retval = RIGHT
+        elif len(x) == 1:
+            if (ord(x) == 68):
+                retval = LEFT
+            elif (ord(x) == 67):
+                retval = RIGHT
+            if (ord(x) == 3):
+                #User pressed Ctrl-C
+                retval = QUIT
     return retval
 
 def next_round(round_number, lane_width, slowness):
@@ -132,10 +193,10 @@ def next_round(round_number, lane_width, slowness):
         modified to make the next round more difficult.
         Actual return data: (round_number, lane_width, slowness)
         '''
-    print('')
-    print('You beat round %d!'%(round_number))
+    myPrint('')
+    myPrint('You beat round %d!'%(round_number))
     round_number = round_number + 1
-    print('Get ready for round %d'%(round_number))
+    myPrint('Get ready for round %d'%(round_number))
     time.sleep(3)
     if round_number % 2:
         #odd rounds change slowness
@@ -146,74 +207,77 @@ def next_round(round_number, lane_width, slowness):
     return (round_number, lane_width, slowness)
 
 
-
 # Set up an object that we will use to get keyboard input from the user.
-getch = _Getch()
+inText = Getch()
 
-# Clear any text from the console screen.
-clear_screen()
+try:
+    # Clear any text from the console screen.
+    clear_screen()
 
-# Set up the initial state of the game.
-center = int(ROW_WIDTH/2)
-lane_width = 30
-slowness = .3
-ski_position = center
+    # Set up the initial state of the game.
+    center = int(ROW_WIDTH/2)
+    lane_width = 30
+    slowness = .3
+    ski_position = center
 
-round_length = 50
-round_number = 1
-i=round_length
-hill_direction = -1
-ski_direction = LEFT
-turn_length = 0
+    round_length = 50
+    round_number = 1
+    i=round_length
+    hill_direction = -1
+    ski_direction = LEFT
+    turn_length = 0
 
-# Start skiing!
-while i>0:
-    i = i-1
-    # Get the lane to ski in.
-    row = build_row(center, lane_width)
-    # Get input from the user
-    ski_direction = get_movement()
-    # Move based on the user input
-    ski_position = handle_movement(ski_position, ski_direction)
-    # Draw the skis onto the row
-    results, row = insert_skis(ski_position, row, ski_direction)
+    # Start skiing!
+    while i>0:
+        i = i-1
+        # Get the lane to ski in.
+        row = build_row(center, lane_width)
+        # Get input from the user
+        ski_direction = get_movement()
+        # Move based on the user input
+        ski_position = handle_movement(ski_position, ski_direction)
+        # Draw the skis onto the row
+        results, row = insert_skis(ski_position, row, ski_direction)
 
-    # Print the row of trees, lane, and skis.
-    print(row)
+        # Print the row of trees, lane, and skis.
+        myPrint(row)
 
-    # Test for collision with the trees.
-    if results == False:
-        print('')
-        print('CRASH!!!!!!')
-        time.sleep(2)
-        break
+        # Test for collision with the trees.
+        if results == False:
+            myPrint('')
+            myPrint('CRASH!!!!!!')
+            inText.cleanup()
+            time.sleep(2)
+            break
 
-    time.sleep(.2)
-    # The following allows us to have periods of no change in our
-    # skiing lane.  It counts down for the next possible change.
-    if turn_length == 0:
-        # Receives random information about the hill we will ski down.
-        turn_length, hill_direction = get_hill_info()
-    else:
-        turn_length = turn_length - 1
+        time.sleep(.2)
+        # The following allows us to have periods of no change in our
+        # skiing lane.  It counts down for the next possible change.
+        if turn_length == 0:
+            # Receives random information about the hill we will ski down.
+            turn_length, hill_direction = get_hill_info()
+        else:
+            turn_length = turn_length - 1
 
-    # If the lane is getting too close to the side of the screen,
-    # then change the direction to go away from that side of the screen.
-    if center == lane_width:
-        hill_direction = 1
-    elif center == ROW_WIDTH - lane_width:
-        hill_direction = -1
+        # If the lane is getting too close to the side of the screen,
+        # then change the direction to go away from that side of the screen.
+        if center == lane_width:
+            hill_direction = 1
+        elif center == ROW_WIDTH - lane_width:
+            hill_direction = -1
 
-    # Change where the center of the lane is located in the row.
-    center = center + hill_direction
+        # Change where the center of the lane is located in the row.
+        center = center + hill_direction
 
-    # At the end of a round, set up for the next round.
-    if i==0:
-        round_number, lane_width, slowness = next_round(round_number, lane_width, slowness)
-        i = round_length
-
-
+        # At the end of a round, set up for the next round.
+        if i==0:
+            round_number, lane_width, slowness = next_round(round_number, lane_width, slowness)
+            i = round_length
 
 
+
+except Exception as e:
+    myPrint('ERROR:', str(e))
+    inText.cleanup()
 
 
